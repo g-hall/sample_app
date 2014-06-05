e95bc1bb51865af6e805804c58858f6b106a3d47
glenn hall <glenn@theperfectagency.org>
2014-06-05 11:31:45 -0400

    update production secret key

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,23 @@
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use existing token
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store in token file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+
+  # secret key base (used by secrets.yml)
+  config.secret_key_base = secure_token
 
   # Code is not reloaded between requests.
   config.cache_classes = true
